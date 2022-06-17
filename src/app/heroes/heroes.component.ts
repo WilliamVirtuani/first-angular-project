a61c6284abda5222530dd4ddb85b9ada64888dda
William Virtuani <william.virtuani@senseisrl.local>
2022-06-17 16:35:18 +0200

    feat: add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +9,20 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero = { id: 0, name: '' };
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   onSelect(hero: Hero) {
     console.log('hero', hero);
     this.selectedHero = hero;
   }
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
